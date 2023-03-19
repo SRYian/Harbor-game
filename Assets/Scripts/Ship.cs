37359dca7ebcc7f7341ccf7de0e403ec838d609d
Ridzki R Alfaza <ridzkiraihanalfaza@gmail.com>
2023-03-20 03:48:21 +0700

    feat: added ship behavior

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -8,12 +8,23 @@ using System.Collections.Generic;
 
 public class Ship : Agent
 {
-    public float Movespeed = 20f;
+    public float maxMoveSpeed = 20f;
+    public float moveSpeed = 0f;
+    public float maxSpeedChange = 1f;
+    public float decelerationMultiplier = 0.3f;
 
-    public float PercentFloorHoles = 0.15f;
+    public float maxTurnSpeed = 15; //degrees
+    public float turnSpeed = 0f;
+
     public float vision_distance = 10f;
 
     private Rigidbody2D rb = null;
+
+    public List<float> visionAngles;
+
+    private Transform targetTransform;
+
+    public float areaSize;
 
     private float episodeTime;
     public float maxTime;
@@ -24,50 +35,82 @@ public class Ship : Agent
     }
     public override void Initialize()
     {
+        targetTransform = transform.parent.Find("Target");
         base.Initialize();
     }
     public override void OnEpisodeBegin()
     {
-
-        //rb.transform.localPosition = new Vector3(UnityEngine.Random.Range(-5, 5), 1, UnityEngine.Random.Range(-5, 5));
-        //rb.transform.rotation = Quaternion.Euler(0, (float)(360 * UnityEngine.Random.value), 0);
         episodeTime = 0;
+        randomizePosition();
         base.OnEpisodeBegin();
     }
     public override void OnActionReceived(ActionBuffers actions)
     {
-        
+        ActionSegment<float> continuousActions = actions.ContinuousActions;
+        AddVel(continuousActions[1]);
+        turnShip(continuousActions[0]);
+
+        base.OnActionReceived(actions);
     }
     public override void CollectObservations(VectorSensor sensor)
     {
         Vector3 p = this.transform.position;
 
+        // position
+        sensor.AddObservation(transform.localPosition.x / 30f); // 2
+        sensor.AddObservation(transform.localPosition.y / 30f);
+        sensor.AddObservation(transform.rotation.z / Mathf.PI); // 1
+
+        sensor.AddObservation((targetTransform.localPosition.x - transform.localPosition.x) / 30f); // 2
+        sensor.AddObservation((targetTransform.localPosition.y - transform.localPosition.y) / 30f);
+        // ship speed
+        sensor.AddObservation(moveSpeed / maxMoveSpeed);
+
+        // vision
         List<bool> vision_observations = new List<bool>();
         Transform tf = rb.transform;
-        
-
+        foreach (float visionAngle in visionAngles) // 3 * 8 = 24
+        {
+            float x = Mathf.Cos(Mathf.Deg2Rad * visionAngle);
+            float y = Mathf.Sin(Mathf.Deg2Rad * visionAngle);
+            RaycastHit hit;
+            bool vision = Physics.Raycast(new Vector3(p.x, p.y, p.z), ((tf.right * x) + (tf.up * y)) * vision_distance, out hit);
+            bool sees_monster = (vision && hit.collider.gameObject.CompareTag("Monster"));
+            bool sees_obstacle = (vision && hit.collider.gameObject.CompareTag("Obstacle"));
+            bool sees_goal = (vision && hit.collider.gameObject.CompareTag("Target"));
+            sensor.AddObservation(sees_monster ? hit.distance / 30f : 1.0f);
+            sensor.AddObservation(sees_obstacle ? hit.distance / 30f : 1.0f);
+            sensor.AddObservation(sees_goal ? hit.distance / 30f: 1.0f);
+        }
+        base.CollectObservations(sensor);
     }
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-       
-    }
-    private void FixedUpdate()
-    {
-        episodeTime += Time.deltaTime;
-        if (episodeTime > maxTime)
-        {
-            AddReward(-0.5f);
-            EndEpisode();
-        }
+        //Heuristic: is a good way to test you actions to see if they work as you would expect
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = 0;  //movement x axis   
+        continuousActionsOut[1] = 0;  //movement z axis
 
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+            continuousActionsOut[0] = -1;
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+            continuousActionsOut[0] = 1;
+
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+            continuousActionsOut[1] = 1;
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+            continuousActionsOut[1] = -1;
+
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+            print("up pressed");
 
     }
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Obstacle") == true)
         {
             // crash
-            AddReward(-0.5f);
+            AddReward(-1f);
             EndEpisode();
             return;
         }
@@ -85,29 +128,58 @@ public class Ship : Agent
             AddReward(1.0f + (maxTime - episodeTime) / maxTime);
             EndEpisode();
         }
+
+        print("collided");
     }
 
-    private void OnTriggerEnter(Collider collision)
-    {
-        //if (collision.gameObject.CompareTag("Target") == true)
-        //{
-        //    AddReward(1.0f + (maxTime - episodeTime) / maxTime);
-        //    EndEpisode();
-        //}
-    }
-    private void OnCollisionExit(Collision collision)
-    {
-        
-    }
     private void OnDrawGizmos()
     {
         Vector3 p = this.transform.position;
+        Transform tf = transform;
+        foreach (float visionAngle in visionAngles)
+        {
+            float x = Mathf.Cos(Mathf.Deg2Rad * visionAngle);
+            float y = Mathf.Sin(Mathf.Deg2Rad * visionAngle);
+            Gizmos.DrawRay(new Vector3(p.x, p.y, p.z), ((tf.right * x) + (tf.up * y)) * vision_distance);
+        }
+        return;
+    }
 
-        //floor sensors (left, right, back, forward)
-        Gizmos.color = Color.magenta;
+    private void FixedUpdate()
+    {
+        episodeTime += Time.deltaTime;
+        if (episodeTime > maxTime)
+        {
+            AddReward(-0.9f);
+            EndEpisode();
+        }
 
-        // down
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawRay(p, Vector2.down);
+        this.transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+        this.transform.rotation *= Quaternion.Euler(0, 0, turnSpeed);
+    }
+
+    private void Update()
+    {
+    }
+
+    private void AddVel(float scale)
+    {
+        float newVal = scale * maxSpeedChange;
+        //float newVal = scale * maxMoveSpeed;
+        if (newVal < 0) newVal *= decelerationMultiplier;
+        moveSpeed = newVal;
+    }
+
+    private void turnShip(float scale)
+    {
+        turnSpeed = -maxTurnSpeed * scale;
+    }
+
+    private void randomizePosition()
+    {
+        rb.transform.localPosition = new Vector3(UnityEngine.Random.Range(-areaSize, areaSize), UnityEngine.Random.Range(-areaSize, areaSize), 0);
+        rb.transform.rotation = Quaternion.Euler(0, 0, (float)(360 * UnityEngine.Random.value));
+
+        targetTransform.gameObject.transform.localPosition = new Vector3(UnityEngine.Random.Range(-areaSize, areaSize), UnityEngine.Random.Range(-areaSize, areaSize), 0);
     }
 }
