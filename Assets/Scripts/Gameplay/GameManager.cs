c7c0bf6c517e70370becd22a9a7ce9be8d61ffe8
SRYian <92420947+SRYian@users.noreply.github.com>
2023-04-02 20:58:50 +0700

    feat: add game pause menu features

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
+    [SerializeField] private GameObject loadingscreen;
+    [SerializeField] private Slider loadingbar;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,12 +28,41 @@ public class GameManager : MonoBehaviour
     {
 
     }
-    void PauseGame()
+    public void PauseGame()
     {
+        Time.timeScale = 0f;
+        AudioListener.pause = true;
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+    }
+
+    public void LoadLevel(int id)
+    {
+        StartCoroutine(loadasync(id));
 
     }
-    void ResumeGame()
-    {
 
+    public void ReturnToMenu()
+    {
+        LoadLevel(0);
     }
+
+    IEnumerator loadasync(int sceneid)
+    {
+        AsyncOperation loading = SceneManager.LoadSceneAsync(sceneid);
+        loadingscreen.SetActive(true);
+        while (!loading.isDone)
+        {
+            float loadvalue = Mathf.Clamp01(loading.progress / 0.9f);
+            loadingbar.value = loadvalue;
+            //Debug.Log("Sudah load " + loadvalue);
+            yield return null;
+        }
+    }
+
+
+
 }
